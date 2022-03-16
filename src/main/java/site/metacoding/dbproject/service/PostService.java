b85b14521e5501880d32f7f3912f13d7ea4b31f7
woals990605 <woals990605@naver.com>
2022-03-16 12:40:06 +0900

    18.서비스 레이어 추가 -트랜잭션 관리

---
+++ src/main/java/site/metacoding/dbproject/service/PostService.java
@@ -0,0 +1,55 @@
+package site.metacoding.dbproject.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.dbproject.domain.post.Post;
+import site.metacoding.dbproject.domain.post.PostRepository;
+import site.metacoding.dbproject.domain.user.User;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Page<Post> 글목록(Integer page) {
+        PageRequest pr = PageRequest.of(page, 3);
+        return postRepository.findAll(pr);
+    }
+
+    // 글수정페이지로 이동할 때도 재활용이 가능하다.
+    public Post 글상세보기(Integer id) {
+        Optional<Post> postOp = postRepository.findById(id);
+
+        if (postOp.isPresent()) {
+            Post postEntity = postOp.get();
+            return postEntity;
+        } else {
+            return null;
+        }
+
+    }
+
+    @Transactional
+    public void 글수정() {
+
+    }
+
+    @Transactional
+    public void 글삭제() {
+
+    }
+
+    @Transactional
+    public void 글쓰기(Post post, User principal) {
+        post.setUser(principal);// User FK 추가!!
+        postRepository.save(post);
+    }
+
+}
--- src/main/java/site/metacoding/dbproject/web/PostController.java
+++ src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -1,11 +1,13 @@
 package site.metacoding.dbproject.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,18 +22,22 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.dbproject.domain.post.Post;
 import site.metacoding.dbproject.domain.post.PostRepository;
 import site.metacoding.dbproject.domain.user.User;
+import site.metacoding.dbproject.service.PostService;
 
-@RequiredArgsConstructor // final이 붙은 애들에 대한 생성자를 만들어준다.
+// 필요한 constructor를 만들어준다. final이 붙은 애들에 대한 생성자를 만들어준다.
+@RequiredArgsConstructor
 @Controller
 public class PostController {
 
     private final HttpSession session;
-    private final PostRepository postRepository;
+    private final PostService postService;
+    private final PostRepository postRepository; // 다하고 지워야됨!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     // GET 글쓰기 페이지 /post/writeForm - 인증 O
     @GetMapping("/s/post/writeForm")
     public String writeForm() {
 
+        // 인증(필터링 처리를 아직 안해서 메서드에서 직접 막아준다.)
         if (session.getAttribute("principal") == null) {
             return "redirect:/loginForm";
         }
@@ -39,78 +45,80 @@ public class PostController {
         return "post/writeForm";
     }
 
-    // 메인페이지 - 인증 X
-    // GET 글목록 페이지 /post/list, /
-    // @GetMapping({"/", "/post/list"})
-    @GetMapping({ "/", "/post/list" })
-    public String list(@RequestParam(defaultValue = "0") Integer page, Model model) {
-
-        // 1.postRepository의 findAll()호출
-
-        // 2.model에 담기
-        // model.addAttribute("posts",
-        // postRepository.findAll(Sort.by(Sort.Direction.DESC, "id")));
-        PageRequest pq = PageRequest.of(page, 3);
-        model.addAttribute("posts", postRepository.findAll(pq));
-        model.addAttribute("prevPage", page - 1);
-        model.addAttribute("nextPage", page + 1);
-        return "post/list";
-    }
-
-    // @GetMapping("/test/post/list")
-    // public @ResponseBody Page<Post> listTest(@RequestParam(defaultValue = "0")
-    // Integer page) {
-    // PageRequest pq = PageRequest.of(page, 3);
-    // return postRepository.findAll(pq);
-    // }
-
-    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨) - 인증 X
-    @GetMapping("/post/{id}") // Get요청에 /post 제외 시키기
-    public String detail(@PathVariable Integer id, Model model) {
-        Optional<Post> postOp = postRepository.findById(id);
-
-        if (postOp.isPresent()) {
-            Post postEntity = postOp.get();
-            model.addAttribute("post", postEntity);
-            return "post/detail";
-        } else {
-            return "error/page1";
-        }
-    }
-
-    // GET 글수정 페이지 /post/{id}/updateForm - 인증 O
-    @GetMapping("/s/post/{id}/updateForm")
-    public String updateForm(@PathVariable Integer id) {
-        return "post/updateForm"; // ViewResolver 도움 받음.
-    }
-
-    // DELETE 글삭제 /post/{id} - 글목록으로 가기 - 인증 O
-    @DeleteMapping("/s/post/{id}")
-    public String delete(@PathVariable Integer id) {
-        return "redirect:/";
-    }
-
-    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기 - 인증 O
-    @PutMapping("/s/post/{id}")
-    public String update(@PathVariable Integer id) {
-        return "redirect:/post/" + id;
-    }
-
-    // POST 글쓰기 /post - 글목록으로 가기 - 인증 O
+    // POST 글쓰기 /post - 인증 O
     @PostMapping("/s/post")
     public String write(Post post) {
+        // System.out.println("title : " + title + ", content : " + content);
 
-        // title, content 1. null검사, 2.공백검사, 3.길이검사 .........
+        // 유효성 검사 패스
 
         if (session.getAttribute("principal") == null) {
             return "redirect:/loginForm";
         }
 
         User principal = (User) session.getAttribute("principal");
-        post.setUser(principal);
-        // insert into post(title, content, userId) values(사용자, 사용자, 세션오브젝트의 PK)
-
-        postRepository.save(post);
+        // 오브젝트를 통째로 넣으면 알아서 foreignkey가 연결된다.
+        postService.글쓰기(post, principal);
         return "redirect:/";
     }
+
+    // 메인페이지 => 보통 주소를 두개를 걸어놓는다.
+    // GET 글목록 페이지 /post/list, / - 인증 X
+    @GetMapping({ "/", "/post/list" })
+    public String list(@RequestParam(defaultValue = "0") Integer page, Model model) {
+        // 1. PostRepository의 findAll() 호출
+        // List<Post> posts = postRepository.findAll();
+        // List<Post> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC,
+        // "id")); // 힌트를 이용해서 거꾸로
+
+        // List<Post> posts = postRepository.findAll(pr);
+
+        // 2. model에 담기
+        Page<Post> pagePosts = postService.글목록(page);
+
+        model.addAttribute("posts", pagePosts);
+        model.addAttribute("nextpage", page + 1);
+        model.addAttribute("prevpage", page - 1);
+
+        return "post/list";
+    }
+
+    @GetMapping("/test/post/list")
+    public @ResponseBody Page<Post> listTest(@RequestParam(defaultValue = "0") Integer page) {
+        PageRequest pq = PageRequest.of(page, 1);
+        return postRepository.findAll(pq);
+    }
+
+    // GET 글 상세보기 페이지 / post/{id} (삭제버튼 만들어 두면됨. 수정버튼 만들어 두면됨) - 인증 X
+    @GetMapping("/post/{id}") // 인증이 필요없기 때문에 주소는 이대로 놔둬야 한다. 따라서 주소는 놔두고 Get요청시에는 필터에서 /post 거르는거를 제외를 시켜준다.
+    public String detail(@PathVariable Integer id, Model model) {
+
+        Post postEntity = postService.글상세보기(id);
+
+        if (postEntity == null) {
+            return "error/page1";
+        } else {
+            model.addAttribute("post", postEntity);
+            return "post/detail";
+        }
+    }
+
+    // GET 글 수정 페이지 /post/{id}/updateForm - 인증 O
+    @GetMapping("/s/post/{id}/updateForm")
+    public String updateForm(@PathVariable Integer id) {
+        return "post/updateForm";
+    }
+
+    // UPDATE 글수정 /post/{id} - 인증 O
+    @PutMapping("/s/post/{id}")
+    public String update(@PathVariable Integer id) {
+        return "redirect:/post/" + id + "/updateForm";
+    }
+
+    // DELETE 글삭제 /post/{id} - 인증 O
+    @DeleteMapping("/s/post/{id}")
+    public String delete(@PathVariable Integer id) {
+        return "redirect:/";
+    }
+
 }
